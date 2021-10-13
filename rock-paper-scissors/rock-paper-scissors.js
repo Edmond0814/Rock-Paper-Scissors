69c904cbb2bfafe97e329a6832864df88c56d1ef
Edmond <edmondyong@mobius-digital.com>
2021-10-13 14:27:53 +0800

    commit to branch

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,8 +1,12 @@
 const select = document.querySelectorAll('button');
+let playerWin=0, computerWin=0;
 
 select.forEach(function(element){
     element.addEventListener('click',function(test){
-        console.log(test.target.parentElement.value);
+        console.log(playRound(test.target.parentElement.value));
+        if (playerWin>=5||computerWin>=5){
+            (playerWin>=5)? alert("The winner is Player!"):alert("The winner is computer!")
+        }
     })
 })
 
@@ -22,23 +26,32 @@ function playRound(playerSelection){
         return("It's a tie!")
     }
     else if (playerSelection=='scissors'){
-        if (computerSelection=='rock')
+        if (computerSelection=='rock'){
+            computerWin++;
             return("You lose! Rock beats Scissors")
+        }
         else{
+            playerWin++;
             return("You win! Scissor beats Paper")
         }
     }
     else if (playerSelection=='paper'){
-        if (computerSelection=='scissors')
+        if (computerSelection=='scissors'){
+            computerWin++;
             return("You lose! Scissor beats Paper")
+        }
         else{
+            playerWin++;
             return("You win! Paper beats Rock")
         }
     }
     else{
-        if (computerSelection=='paper')
+        if (computerSelection=='paper'){
+            computerWin++;
             return("You lose! Paper beats Rock")
+        }
         else{
+            playerWin++;
             return("You win! Rock beats Scissor")
         }
     }
